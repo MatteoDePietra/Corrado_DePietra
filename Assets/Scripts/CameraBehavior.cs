20f9439b73c79d663059fa23b1babf736ab13b02
MatteoDePietra <55741771+MatteoDePietra@users.noreply.github.com>
2020-04-27 13:06:47 +0200

    1.0.2
    - MainCamera follow player
    - Background loop
    - Background parallax
    - Myst scroll

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -5,47 +5,49 @@ using UnityEngine;
 public class CameraBehavior : MonoBehaviour
 {
     private Transform target;
-    private float targetSmooth = .1f;
-    private Vector3 targetOffset;
+    private float targetSmooth = .125f;
 
     [SerializeField]
     private Transform[] backgrounds = null;
     private float[] parallaxScale;
-    private float parallaxsmooth = 2f;
+    private float parallaxsmooth = 10f;
     private Vector3 previousCamPos;
 
     [SerializeField]
     private Transform myst = null;
-    private float mystSmooth = .5f;
+    private float mystSmooth = 5f;
     private Vector3 mystOffset;
 
     private void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
-        targetOffset = new Vector3(0, .5f, -10);
 
         previousCamPos = transform.position;
         parallaxScale = new float[backgrounds.Length];
         for (int i = 0; i < backgrounds.Length; i++)
         {
-            parallaxScale[i] = (backgrounds[i].position.z - 1f)/5;
-            Debug.Log(i + ": " + parallaxScale[i]);
+            parallaxScale[i] = (backgrounds[i].position.z - 1f)/15;
         }
 
-        mystOffset = new Vector3(.1f, 0, 0);
+        mystOffset = new Vector3(-.01f, 0, 0);
     }
     
-    private void FixedUpdate()
+    private void LateUpdate()
     {
         FollowTarget();
         ParallaxBackground();
         MystBackground();
     }
 
+    private void FixedUpdate()
+    {
+    }
+
     private void FollowTarget()
     {
-        Vector3 desiredPosition = target.position + targetOffset;
-        transform.position = Vector3.Lerp(transform.position, desiredPosition, targetSmooth);
+        Vector3 desiredPosition = new Vector3(target.position.x, transform.position.y, transform.position.z);
+        Vector3 newPosition = Vector3.Lerp(transform.position, desiredPosition, targetSmooth);
+        transform.position = newPosition;
     }
 
     private void ParallaxBackground()
